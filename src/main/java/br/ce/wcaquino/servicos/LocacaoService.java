7c0c8d8b07c3e0cc19194d1838fdbd47bbb0a0df
Ruan Silva <ruan.silva@widesoftware.com.br>
2019-10-17 10:43:41 -0300

    teste com junit

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -4,6 +4,9 @@ import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
@@ -29,7 +32,8 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void teste() {
 		//cenário
 		LocacaoService locacaoService = new LocacaoService();
 		Usuario usuario = new Usuario("Raj");
@@ -44,8 +48,11 @@ public class LocacaoService {
 //		System.out.println(locacao.getDataLocacao());
 //		System.out.println(locacao.getDataRetorno());
 		
-		System.out.println(locacao.getValor() == 12.9);
-		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+//		System.out.println(locacao.getValor() == 12.9);
+//		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+//		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+		Assert.assertTrue(locacao.getValor() == 12.9);
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
 	}
 }
